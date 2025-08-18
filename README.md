1f09eb91500a90291607b39bd88dcc78d27ce84c
dailamoss <persida-solange@hotmail.com>
2025-08-18 19:52:27 -0400

    Primer commit: proyecto Amigo Secreto

---
+++ README.md
@@ -0,0 +1,15 @@
+# 🎁 Amigo Secreto
+Aplicación sencilla en JavaScript que permite ingresar nombres y realizar un sorteo para elegir al **amigo secreto**
+
+## 🚀 Cómo usar
+1. Ingresa los nombres de tus amigos en el campo de texto.
+2. Presiona el botón **Añadir amigos** para añadirlos a la lista.
+3. Una vez ingresados, presiona **Sortear amigo** y aparecerá el resultado.
+
+## 🛠️ Tecnologías utilizadas
+- HTML
+- CSS
+- JavaScript
+
+## 👩‍💻 Autor
+Persida Retamal
---
+++ app.js
@@ -0,0 +1,42 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+//funcion para agregar un amigo a la lista
+function agregarAmigo() {
+    let input = document.getElementById( "amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("por favor, escribe un nombre valido.");
+        return;
+
+    }
+    amigos.push(nombre);
+    input.value = "";
+    mostrarLista ();
+}
+
+//funcion para mostrar la lista en el HTML
+function mostrarLista() {
+    let lista = document.getElementById ("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach(function (amigos){
+        let li = document.createElement("li");
+        li.textContent = amigos;
+        lista.appendChild(li);
+    });
+}
+
+// funcion para sortear un amigo al azar
+function sortearAmigo (){
+    if (amigos.length < 2){
+        alert( " debes ingresar al menos 2 amigos para el sorteo.");
+        return;
+    }
+    let indiceAleatorio = Math.floor (Math.random () * amigos.length);
+    let amigoSorteado = amigos [indiceAleatorio];
+
+    let resulado = document.getElementById ("resultado");
+    resultado.innerHTML = "El amigo secreto es: " + amigoSorteado;
+}
