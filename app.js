961b0f6fd79905ffea40ab55a04109eb75aae42f
dailamoss <persida-solange@hotmail.com>
2025-08-18 21:27:49 -0400

    limpiar comentario inicial y mejorar documentacion

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// almacenar los nombres de los amigos
 let amigos = [];
 
 //funcion para agregar un amigo a la lista
